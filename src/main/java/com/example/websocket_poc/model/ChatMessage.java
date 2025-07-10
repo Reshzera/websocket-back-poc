f7ebfe114e15de007083be8041387048cb8d53b4
Reshzera <reoshiro13@gmail.com>
2025-07-10 19:35:19 -0400

    feat: add changes on websocket implemntation

--- src/main/java/com/example/websocket_poc/model/ChatMessage.java
+++ src/main/java/com/example/websocket_poc/model/ChatMessage.java
@@ -1,6 +1,29 @@
 package com.example.websocket_poc.model;
 
 public class ChatMessage {
-    private String from;
+    private String senderId;
     private String text;
+
+    public ChatMessage() {} // construtor vazio obrigatório
+
+    public ChatMessage(String senderId, String text) {
+        this.senderId = senderId;
+        this.text = text;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
