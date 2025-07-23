bb6322d19934e7dd5ef497e0e4061943ba06ba0b
Reshzera <reoshiro13@gmail.com>
2025-07-23 10:04:50 -0400

    feat: add auth token

--- src/main/java/com/example/websocket_poc/config/SocketIOConfig.java
+++ src/main/java/com/example/websocket_poc/config/SocketIOConfig.java
@@ -5,6 +5,8 @@ import com.corundumstudio.socketio.SocketIOServer;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Bean;
+import com.corundumstudio.socketio.AuthorizationResult;
+
 
 @org.springframework.context.annotation.Configuration
 public class SocketIOConfig {
@@ -16,6 +18,15 @@ public class SocketIOConfig {
         Configuration config = new Configuration();
         config.setHostname("localhost");
         config.setPort(8081);
+
+        config.setAuthorizationListener(handshakeData -> {
+            String token = handshakeData.getSingleUrlParam("token");
+
+            if (token == null || !isValidToken(token)) {
+                return AuthorizationResult.FAILED_AUTHORIZATION;
+            }
+            return AuthorizationResult.SUCCESSFUL_AUTHORIZATION;
+        });
 
         server = new SocketIOServer(config);
         server.start();
@@ -29,5 +40,9 @@ public class SocketIOConfig {
             server.stop();
             System.out.println("🛑 Socket.IO server stopped");
         }
+    }
+
+    private boolean isValidToken(String token) {
+        return token.equals("token-auth");
     }
 }
